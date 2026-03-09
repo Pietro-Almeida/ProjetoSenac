53020b781b6e6662878bd7cc904fd3d001d32d11
Ragnamellia <lucas58187536@edu.df.senac.br>
2026-03-09 16:15:58 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,7 @@
 const nome = document.getElementById("nomeCompleto");
 const cpf = document.getElementById("CPF");
 const telefone = document.getElementById("Telefone");
-const dataNascimento = document.getElementById("DataNascimento");
+const dataNascimento = document.getElementById("DataNascimento");const dataNascimento = document.getElementById("DataNascimento");
+const email = document.getElementById("Email");
+const endereço = document.getElementById("Endereço");
+const profissão = document.getElementById("Profissão");
