3d8b5cd9c29d64ac21e3ad2f071e9c6af3a5f0e2
Pietro de Almeida Alves Silva <pietro.almeidaalves83@gmail.com>
2026-03-09 17:05:50 -0300

    fjdkslaçfjdskl

--- js/main.js
+++ js/main.js
@@ -1,17 +1,3 @@
-function mudarTema(){
-  let body = document.body
-  body.classList.toggle("dark")
-  // salvar tema
-  if(body.classList.contains("dark")){
-    localStorage.setItem("tema","dark")
-  }else{
-    localStorage.setItem("tema","light")
-  }
-}
-  // carregar tema salvo
-  window.onload = function(){
-  let tema = localStorage.getItem("tema")
-  if(tema === "dark"){
-    document.body.classList.add("dark")
-}
-}
+const btnTema = document.getElementById("toggle-theme").addEventListener("click", ()=> {
+  document.body.classList.toggle("black")
+})
