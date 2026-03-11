c5a9df2370a0f7a50c9f09cc21d4b8d23f097e34
Pietro de Almeida Alves Silva <pietro.almeidaalves83@gmail.com>
2026-03-11 16:42:36 -0300

    modo escuro feito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-const btnTema = document.getElementById("toggle-theme").addEventListener("click", ()=> {
+const body = document.body
+const btnTema = document.getElementById("toggle-theme")
+
+
+btnTema.addEventListener("click", ()=> {
   document.body.classList.toggle("black")
+  btnTema.innerHTML = document.body.classList.contains("black") ? "☀️" : "🌙"
 })
