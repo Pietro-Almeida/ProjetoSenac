d4829992b257aa6d90aeabfb1bf7630c5160fba3
Pietro de Almeida Alves Silva <pietro.almeidaalves83@gmail.com>
2026-03-13 16:36:38 -0300

    login associado

---
+++ js/associado.js
@@ -0,0 +1,22 @@
+export function loginAssociado() {
+
+    const usuario = document.getElementById("cpf").value;
+    const senha = document.getElementById("senha").value;
+
+    // Credenciais fixas
+    const usuarioCorreto = "usuario";
+    const senhaCorreta = "senha";
+
+    if (usuario === usuarioCorreto && senha === senhaCorreta) {
+
+        alert("Login realizado com sucesso!");
+
+        // redireciona para o painel do administrador
+        window.location.href = "associado.html";
+
+    } else {
+
+        alert("Usuário ou senha incorretos!");
+
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 import { loginAdmin } from "./admin.js";
+import { loginAssociado } from "./associado.js";
+
 const body = document.body
 const btnTema = document.getElementById("toggle-theme")
 const buttonLoginAdmin = document.getElementById("loginAdmin")
+const buttonLoginAssociado = document.getElementById("loginAssociado")
 
 btnTema.addEventListener("click", ()=> {
   document.body.classList.toggle("black")
   btnTema.innerHTML = document.body.classList.contains("black") ? "☀️" : "🌙"
 })
-buttonLoginAdmin.addEventListener("click", ()=> {
-  loginAdmin();
-})
+
+if(buttonLoginAdmin)
+  buttonLoginAdmin.addEventListener("click", ()=>{
+    loginAdmin();
+  })
+if(buttonLoginAssociado){
+  buttonLoginAssociado.addEventListener("click", ()=>{
+    loginAssociado();
+  })
+}
 
