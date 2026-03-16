4e443b8b302bbbfaae015bf2507cd453e6506c85
Pietro de Almeida Alves Silva <pietro.almeidaalves83@gmail.com>
2026-03-16 17:01:33 -0300

    admin finalizado

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,5 @@
+const btnToggleTheme = document.getElementById("toggle-theme")
+
 export function loginAdmin() {
 
     const usuario = document.getElementById("usuario").value;
@@ -20,3 +22,44 @@ export function loginAdmin() {
 
     }
 }
+
+
+/* GRÁFICO CONTRIBUIÇÕES */
+
+new Chart(document.getElementById("contribChart"),{
+type:"bar",
+data:{
+labels:["Jan","Fev","Mar","Abr","Mai","Jun"],
+datasets:[{
+label:"Contribuições",
+data:[35000,42000,39000,45000,48000,52000]
+}]
+}
+})
+
+/* GRÁFICO RELATORIO */
+
+new Chart(document.getElementById("financeChart"),{
+type:"line",
+data:{
+labels:["Out","Nov","Dez","Jan","Fev","Mar"],
+datasets:[{
+label:"Receita",
+data:[42000,47000,45000,52000,55000,58000],
+borderWidth:3,
+fill:false
+}]
+}
+})
+
+btnToggleTheme.addEventListener("click", ()=> {
+
+document.body.classList.toggle("dark-mode");
+
+if(document.body.classList.contains("dark-mode")){
+toggleTheme.textContent="☀️ Modo Claro";
+}else{
+toggleTheme.textContent="🌙 Modo Escuro";
+}
+
+}); 
